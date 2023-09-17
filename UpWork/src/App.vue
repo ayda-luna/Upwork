8ed878cf417f978e51517be8d294d6a9d49761bf
ayda-luna <lunaayda888@gmail.com>
2023-09-17 12:46:09 +0330

    v3

--- UpWork/src/App.vue
+++ UpWork/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
-    <v-row no-gutters>
+    <v-row no-gutters align="center">
       <v-col cols="12">
         <v-sheet class="pa-6">
           <Navbar></Navbar>
         </v-sheet>
       </v-col>
-      <v-col cols="12">
-        <v-sheet class="pa-6">
+      <v-col cols="4">
+        <v-sheet class="pa-4 ma-4">
           <Menubar></Menubar>
         </v-sheet>
       </v-col>
--- UpWork/src/components/Menubar.vue
+++ UpWork/src/components/Menubar.vue
@@ -1,60 +1,35 @@
 <template>
-    <div>
-        <v-menu>
-            <template #activator="{ props }">
-              <v-btn variant="tonal" v-bind="props" class="text-h6">
-                Activator slot <v-icon icon="mdi-chevron-down"></v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(item, index) in items"
-                :key="index"
-                :value="index"
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu><v-menu>
-            <template #activator="{ props }">
-              <v-btn variant="tonal" v-bind="props" class="text-h6">
-                Activator slot <v-icon icon="mdi-chevron-down"></v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(item, index) in items"
-                :key="index"
-                :value="index"
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu><v-menu>
-            <template #activator="{ props }">
-              <v-btn variant="tonal" v-bind="props" class="text-h6">
-                Activator slot <v-icon icon="mdi-chevron-down"></v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(item, index) in items"
-                :key="index"
-                :value="index"
-              >
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-    </div>
+  <div  class="menubar mx-16 px-13">
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn variant="tonal" v-bind="props" class="text-h6">
+          Activator slot 
+        </v-btn>
+      </template>
+    </v-menu>
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn variant="tonal" v-bind="props" class="text-h6">
+          Activator slot 
+        </v-btn>
+      </template>
+    </v-menu>
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn variant="tonal" v-bind="props" class="text-h6">
+          Activator slot
+        </v-btn>
+      </template>
+    </v-menu>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.menubar{
+    width: 100rem;
+}
 </style>
